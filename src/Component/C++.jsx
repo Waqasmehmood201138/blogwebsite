a772ed6a0306e18bd3caff6ff976223b4da865f7
Waqas Mehmood <95920177+Waqasmehmood201138@users.noreply.github.com>
2022-10-20 20:04:31 +0500

    c++ page setup done

--- src/Component/C++.jsx
+++ src/Component/C++.jsx
@@ -1,40 +1,86 @@
 import React from 'react'
+import Card from '../Card'
 
-import Card from './Component/Card';
-import Sample from './Component/Sample';
+import Sample from '../Sample'
 
-
-
+console.log(Sample[0].title);
 
 export default function Cplusplus() {
+
+  
+
   return (
     <>
-      <div >
+
+    
+      <div className='container my-3'>
+
+        <div className='row'>
+          <div className='col-md-3'>
+            <Card title={Sample[0].title} description={Sample[0].description}  imageUrl={Sample[0].urlToImage}/>
+          </div>
+        
+          
+        
+          <div className='col-md-3'>
+          <Card title={Sample[2].title} description={Sample[2].description}  imageUrl={Sample[2].urlToImage}/>
+          </div>
+       
+
+        
+          <div className='col-md-3'>
+          <Card title={Sample[3].title} description={Sample[3].description}  imageUrl={Sample[3].urlToImage}/>
+          </div>
 
 
-        <div className='container my-3'>
+        
+          </div>
+
 
           <div className='row'>
-
-
-            {Sample.map((value, index) => {
-
-              return <div className='col-md-3' key={index}>
-
-                <Card
-                  urlToImage={value.urlToImage}
-                  title={value.title}
-                  description={value.description} />
-
-              </div>
-            })}
-
-            
-
-
+          <div className='col-md-3'>
+            <Card title={Sample[4].title} description={Sample[4].description}  imageUrl={Sample[4].urlToImage}/>
           </div>
-        </div>
+        
+          
+        
+          <div className='col-md-3'>
+          <Card title={Sample[5].title} description={Sample[5].description}  imageUrl={Sample[5].urlToImage}/>
+          </div>
+       
+
+        
+          <div className='col-md-3'>
+          <Card title={Sample[6].title} description={Sample[6].description}  imageUrl={Sample[6].urlToImage}/>
+          </div>
+
+          
+        
+          </div>
+
+
+          <div className='row'>
+          <div className='col-md-3'>
+            <Card title={Sample[7].title} description={Sample[7].description}  imageUrl={Sample[7].urlToImage}/>
+          </div>
+        
+          
+        
+          <div className='col-md-3'>
+          <Card title={Sample[8].title} description={Sample[8].description}  imageUrl={Sample[8].urlToImage}/>
+          </div>
+       
+
+        
+          <div className='col-md-3'>
+          <Card title={Sample[9].title} description={Sample[9].description}  imageUrl={Sample[9].urlToImage}/>
+          </div>
+
+          
+        
+          </div>
       </div>
+
     </>
   )
 }
--- src/Component/Java.jsx
+++ src/Component/Java.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+
 export default function Java() {
   return (
     <>
    <div>
    <h1  className='text-primary text-center my-5'>There Will be Java Blogs</h1>
     <h4 className='text-center'>Comming Soon...</h4>
+
+
+
    </div>
    </>
   )
